a3f64d4cff5129fece51eb660aeda80c36eb7163
OgabekYuldoshev <yuldashoff1@gmail.com>
2024-12-20 14:40:48 +0500

    style(lint): :lipstick: format code

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,14 +1,14 @@
 import { AppSidebar } from "@/components/app-sidebar";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import React, { type PropsWithChildren } from "react";
 export default function RootLayout({ children }: PropsWithChildren) {
-    return (
-        <SidebarProvider>
-            <AppSidebar />
-            <main>
-                {children}
-                <SidebarTrigger />
-            </main>
-        </SidebarProvider>
-    );
+	return (
+		<SidebarProvider>
+			<AppSidebar />
+			<main>
+				{children}
+				<SidebarTrigger />
+			</main>
+		</SidebarProvider>
+	);
 }
--- src/components/app-sidebar-footer.tsx
+++ src/components/app-sidebar-footer.tsx
@@ -1,79 +1,84 @@
-'use client';
+"use client";
 
 import {
-
-    SidebarFooter,
-
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
-} from "@/components/ui/sidebar"
+	SidebarFooter,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from "@/components/ui/sidebar";
 import { authClient } from "@/lib/auth-client";
 import consola from "consola";
-import { ChevronUp, LogOut, User2 } from "lucide-react"
+import { ChevronUp, LogOut, User2 } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
 import { toast } from "sonner";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu"
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 export type AppSidebarFooterProps = {
-    email: string,
-    name: string
+	email: string;
+	name: string;
 };
 export function AppSidebarFooter(props: AppSidebarFooterProps) {
-    const { email, name } = props
+	const { email, name } = props;
 
-    async function signOut() {
-        const { error, data } = await authClient.signOut()
+	async function signOut() {
+		const { error, data } = await authClient.signOut();
 
-        if (error) {
-            toast.error(error.message)
-            consola.error(error)
-            return;
-        }
+		if (error) {
+			toast.error(error.message);
+			consola.error(error);
+			return;
+		}
 
-        if (data.success) {
-            redirect('/auth')
-        }
-    }
-    return (
-        <SidebarFooter>
-            <SidebarMenu>
-                <SidebarMenuItem>
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <SidebarMenuButton className="h-auto">
-                                <div className="flex items-center">
-                                    <User2 size={18} />
-                                    <div className="flex flex-col ml-1 group-data-[state=collapsed]:hidden">
-                                        <h4 className="text-sm">{name}</h4>
-                                        <span className="text-xs text-muted-foreground">{email}</span>
-                                    </div>
-                                </div>
-                                <ChevronUp className="ml-auto" />
-                            </SidebarMenuButton>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent
-                            side="top"
-                            className="w-[--radix-popper-anchor-width]"
-                        >
-                            <DropdownMenuItem>
-                                <Link href="/account" className="flex items-center">
-                                    <User2 size={18} className="mr-2" />
-                                    Account
-                                </Link>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem onClick={signOut}>
-                                <span className="text-destructive flex items-center">
-                                    <LogOut size={18} className="mr-2" />
-                                    Logout
-                                </span>
-                            </DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                </SidebarMenuItem>
-            </SidebarMenu>
-        </SidebarFooter>
-    );
+		if (data.success) {
+			redirect("/auth");
+		}
+	}
+	return (
+		<SidebarFooter>
+			<SidebarMenu>
+				<SidebarMenuItem>
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<SidebarMenuButton className="h-auto">
+								<div className="flex items-center">
+									<User2 size={18} />
+									<div className="flex flex-col ml-1 group-data-[state=collapsed]:hidden">
+										<h4 className="text-sm">{name}</h4>
+										<span className="text-xs text-muted-foreground">
+											{email}
+										</span>
+									</div>
+								</div>
+								<ChevronUp className="ml-auto" />
+							</SidebarMenuButton>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent
+							side="top"
+							className="w-[--radix-popper-anchor-width]"
+						>
+							<DropdownMenuItem>
+								<Link href="/account" className="flex items-center">
+									<User2 size={18} className="mr-2" />
+									Account
+								</Link>
+							</DropdownMenuItem>
+							<DropdownMenuItem onClick={signOut}>
+								<span className="text-destructive flex items-center">
+									<LogOut size={18} className="mr-2" />
+									Logout
+								</span>
+							</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</SidebarMenuItem>
+			</SidebarMenu>
+		</SidebarFooter>
+	);
 }
