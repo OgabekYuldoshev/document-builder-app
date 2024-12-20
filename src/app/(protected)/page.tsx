ab9fda09d2bf3bf7605647a9b139b2f32a002386
OgabekYuldoshev <yuldashoff1@gmail.com>
2024-12-20 14:40:29 +0500

    style(sidebar): :lipstick: make sidebar and login and logout sections done

---
+++ src/app/(protected)/page.tsx
@@ -0,0 +1,7 @@
+import { getUserSession } from "@/lib/auth";
+
+export default async function Page() {
+    const session = await getUserSession();
+
+    return <div>{session && <pre>{JSON.stringify(session, null, 2)}</pre>}</div>;
+}
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getUserSession } from "@/lib/auth";
-
-export default async function Page() {
-	const session = await getUserSession();
-
-	return <div>{session && <pre>{JSON.stringify(session, null, 2)}</pre>}</div>;
-}
