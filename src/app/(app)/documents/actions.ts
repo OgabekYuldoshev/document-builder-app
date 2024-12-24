a4128887785fdaa816cb396d340d9f1102f88ece
OgabekYuldoshev <yuldashoff1@gmail.com>
2024-12-24 12:01:48 +0500

    feat(document): :sparkles: document creation and document deletion is done
    \

---
+++ src/app/(app)/documents/actions.ts
@@ -0,0 +1,76 @@
+"use server";
+
+import { existsSync } from "node:fs";
+import { mkdir, rm, writeFile } from "node:fs/promises";
+import path from "node:path";
+import { getUserSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { DEFAULT_DOCUMENT_CONTENT } from "@/utils/constants";
+import { constantCase } from 'change-case'
+import { revalidatePath } from "next/cache";
+import type { DocumentFormValue } from "./create-new-document";
+
+function getDocumentPath() {
+    const projectDir = process.cwd()
+    const documentDir = path.join(projectDir, '.documents')
+    return documentDir
+}
+export async function createDocumentAction(values: DocumentFormValue) {
+    try {
+        const documentDir = getDocumentPath()
+        if (!existsSync(documentDir)) {
+            await mkdir(documentDir)
+        }
+        values.key = constantCase(values.key)
+        const documentDb = await db.document.findUnique({
+            where: { key: values.key }
+        })
+        if (documentDb) throw new Error("Document is already created, please use another key to create a new one")
+        const documentPath = path.join(documentDir, `${values.key}.html`)
+        await writeFile(documentPath, DEFAULT_DOCUMENT_CONTENT)
+        const session = await getUserSession()
+        if (!session) throw new Error("User session is not available")
+
+        const newDocument = await db.document.create({
+            data: {
+                key: values.key,
+                name: values.name,
+                userId: session.user.id
+            }
+        })
+
+        return {
+            data: newDocument,
+            error: null
+        }
+    } catch (error) {
+        return {
+            data: null,
+            error: (error as Error).message
+        }
+    }
+}
+
+
+export async function deleteDocumentAction({ id }: { id: string }) {
+    try {
+        const document = await db.document.delete({
+            where: { id }
+        })
+        const documentDir = getDocumentPath()
+        const documentPath = path.join(documentDir, `${document.key}.html`)
+        if (existsSync(documentPath)) {
+            await rm(documentPath)
+        }
+        revalidatePath("/documents")
+        return {
+            data: document.id,
+            error: null
+        }
+    } catch (error) {
+        return {
+            data: null,
+            error: (error as Error).message
+        }
+    }
+}
---
+++ src/app/(app)/documents/columns.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import type { Document } from "@prisma/client";
+
+import { normalizeDate } from "@/utils/date";
+
+import { TableAction, TableActionItem } from "@/components/ui/table";
+import type { ColumnDef } from "@tanstack/react-table";
+import Link from "next/link";
+import { deleteDocumentAction } from "./actions";
+
+export const columns: ColumnDef<Document & { user: { name: string } }>[] = [
+    {
+        accessorKey: "id",
+        header: "Id",
+    },
+    {
+        accessorKey: "key",
+        header: "Key",
+    },
+    {
+        accessorKey: "name",
+        header: "Name",
+    },
+    {
+        accessorFn: (item) => item.user.name,
+        header: "Email",
+    },
+    {
+        accessorFn: (item) => normalizeDate(item.createdAt),
+        header: "Created at",
+    },
+    {
+        id: "actions",
+        enableHiding: false,
+        cell: ({ row }) => {
+            return (
+                <TableAction>
+                    <TableActionItem asChild>
+                        <Link href={`/documents/${row.getValue("id")}`}>View</Link>
+                    </TableActionItem>
+                    <TableActionItem
+                        onClick={() => deleteDocumentAction({ id: row.getValue("id") })}
+                    >
+                        Delete
+                    </TableActionItem>
+                </TableAction>
+            );
+        },
+    },
+];
